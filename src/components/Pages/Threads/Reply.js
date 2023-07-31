9a5da2deba37c03e902759c03af476149d596f2e
aditya <rawatas0901@gmail.com>
2023-07-31 21:45:25 +0530

    fixed the nested replies ui

--- src/components/Pages/Threads/Reply.js
+++ src/components/Pages/Threads/Reply.js
@@ -23,34 +23,39 @@ const Reply = ({ reply }) => {
 
   return (
     <>
-      <div className="mt-3 bg-[#fafafa] flex gap-1 p-2 rounded-lg dark:bg-[#0E162A]">
-        <div className="avatar min-w-[3rem] md:w-[5%] flex justify-center pt-2 mr-2">
-          <Avatar name={reply?.user} size="40" round={true} />
-        </div>
-        <div className="flex flex-col w-full">
-          <div className="reply_head flex gap-2 text-[0.7rem] text-gray-600 dark:text-gray-400">
-            <p>@{reply?.user}</p>
-            <p>{formatDistanceToNow(new Date(reply?.createdAt))}</p>
-          </div>
-          <div className="reply_body w-full">
-            <p className="text-[0.8rem]">{reply?.text}</p>
+      <div className="mt-3 bg-[#fafafa] md:flex flex-col gap-1 p-2 rounded-lg dark:bg-[#0E162A]">
+        <div className="md:flex flex-col w-full md:ml-2 ml-1">
+          <div className="flex items-center gap-2">
+            <div className="avatar md:min-w-[3rem] md:w-[5%] md:flex justify-center w-min">
+              <Avatar name={reply?.user} size="40" round={true} />
+            </div>
+            <div className="reply_head flex flex-col gap-2 text-[0.7rem] text-gray-600 dark:text-gray-400">
+              <div className="flex gap-2">
+                <p>@{reply?.user}</p>
+                <p>{formatDistanceToNow(new Date(reply?.createdAt))}</p>
+              </div>
+              <div className="reply_body w-full">
+                <p className="text-[0.8rem]">{reply?.text}</p>
+              </div>
+            </div>
           </div>
           <div>
             <button
               onClick={() => commentBoxHandler()}
-              className="text-[1rem] w-min  hover:bg-[#c4c4c4] py-1 px-3 mt-5 rounded-2xl dark:hover:text-black mr-2"
+              className="text-[1rem] w-min  hover:bg-[#c4c4c4] py-1 px-3 mt-2 rounded-2xl dark:hover:text-black mr-2"
             >
               Reply
             </button>
             {data?.length > 0 ? (
               <button
                 onClick={() => setShowReplies((prev) => !prev)}
-                className="text-[1rem] w-max  hover:bg-[#c4c4c4] py-1 px-3 mt-5 rounded-2xl dark:hover:text-black"
+                className="text-[1rem] w-max  hover:bg-[#c4c4c4] py-1 px-3 mt-2 rounded-2xl dark:hover:text-black"
               >
                 {showReplies ? "Hide Replies" : "Show Replies"}
               </button>
             ) : null}
           </div>
+
           <div ref={commentBoxRef} className="reply_box mt-2 hidden">
             <Comment
               commentBoxRef={commentBoxRef}
@@ -58,12 +63,12 @@ const Reply = ({ reply }) => {
               currentReply={reply}
             />
           </div>
-          {data?.length > 0 && showReplies
-            ? data?.map((reply) => {
-                return <Reply key={reply.replyNumber} reply={reply} />;
-              })
-            : null}
         </div>
+        {data?.length > 0 && showReplies
+          ? data?.map((reply) => {
+              return <Reply key={reply.replyNumber} reply={reply} />;
+            })
+          : null}
       </div>
     </>
   );
